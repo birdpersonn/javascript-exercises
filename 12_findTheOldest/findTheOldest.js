445aca11f483a34559cf30526b06535f7aa176d9
birdpersonn <chanelcarp@gmail.com>
2022-12-20 14:16:53 -0800

    complete exercises 8-12; all tests passing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.map((person) => {
+        if(person["yearOfDeath"]) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return new Date().getFullYear() - person.yearOfBirth;
+        }
+    });
+    
+    const max = Math.max(...ages);
+    const i = ages.indexOf(max);
+    console.log(people[i]);
+    return people[i];
 };
 
 // Do not edit below this line
